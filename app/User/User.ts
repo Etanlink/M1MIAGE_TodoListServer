fce7344e56132bb7f0a60224e703e6a4f8c83364
Etienne OLIVIER <Etanlink@users.noreply.github.com>
2017-11-05 15:19:26 +0100

    Add the default states of list

--- app/User/User.ts
+++ app/User/User.ts
@@ -46,11 +46,19 @@ export class User {
     private clients: Client[] = [];
     constructor(private passport: PassportUser) {
         users.set(passport.id, this);
+        /* Ajout de la liste de tâches par défaut */
         this.SERVER_CREATE_NEW_LIST( null,  {
             type: "SERVER_CREATE_NEW_LIST",
             name: "Tâches",
             data: {},
             clientListId: "toto"
+        });
+        /* Ajout de la liste de tâches Faites */
+        this.SERVER_CREATE_NEW_LIST( null,  {
+            type: "SERVER_CREATE_NEW_LIST",
+            name: "Faites",
+            data: {},
+            clientListId: "titi"
         });
     }
     dispose() {
